c2ada52eab338b42f1d714070e078a77984a6cda
vidhanshu borade <vidhanshu7218555039@gmail.com>
2022-07-06 15:58:25 +0530

    quick sort added

--- sorting.cpp/quick sort/quick.cpp
+++ sorting.cpp/quick sort/quick.cpp
@@ -1,12 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void partition(int *ar, int n)
+int partition(int *ar, int l, int h)
 {
+    int &pivot = ar[l];
+    int i = l;
+    int j = h;
+    while (i < j)
+    {
+        do
+        {
+            i++;
+        } while (ar[i] <= pivot);
+        do
+        {
+            j--;
+        } while (ar[j] > pivot);
+        if (i < j)
+        {
+            swap(ar[i], ar[j]);
+        }
+    }
+    swap(pivot, ar[j]);
+    return j;
 }
 
-void quick_sort(int *ar, int n)
+void quick_sort(int *ar, int l, int h)
 {
+    if (l < h)
+    {
+        int j = partition(ar, l, h);
+        quick_sort(ar, l, j);
+        quick_sort(ar, j + 1, h);
+    }
 }
 
 void print(int *ar, int n)
@@ -18,10 +44,10 @@ void print(int *ar, int n)
 }
 int main()
 {
-    int ar[] = {2, 3, -12, 992, 233, 23};
+    int ar[] = {2, 3, -12, 992, 233, 23, INT_MAX};
     int n = sizeof(ar) / sizeof(int);
-    quick_sort(ar, n);
+    quick_sort(ar, 0, n - 1);
     cout << "\n";
-    print(ar, n);
+    print(ar, n - 1);
     return 0;
 }
